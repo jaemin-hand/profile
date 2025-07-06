396c1ad895362270d65c3646dbe1e7ee200663e8
손재민 <thswoalsrns@naver.com>
2025-07-07 02:53:14 +0900

    Fix:index.html

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,10 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>안녕하세요 deploy test 해봅니다2 --rebase 테스트.</h1>
+      <h1>안녕하세요.</h1>
     </>
   )
 }
