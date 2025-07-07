0439c11424110de9a6bb435a8cf50a46e58dc53d
손재민 <thswoalsrns@naver.com>
2025-07-08 00:47:06 +0900

    Setting: tailwind 4.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
 
   return (
     <>
-      <h1>안녕하세요.</h1>
+      <h1 className='text-4xl font-bold text-blue-500 underline'>안녕하세요.</h1>
     </>
   )
 }
